b57ada793a6d1e5804fd02d841ae6dff427cfe2c
prakash <prakashchoure@gmail.com>
2023-07-01 11:21:20 +0530

    login_using_mongodb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [form, setForm] = useState({});
+  
+  const [ ,setUsers] = useState([]);
+
+  const handleForm = (e)=>{
+    setForm({
+      ...form,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/demo',{
+      method:'POST',
+      body:JSON.stringify(form),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    const data = await response.json();
+   console.log(data);
+  }
+
+  const getUsers = async ()=>{
+    const response = await fetch('http://localhost:8080/demo',{
+      method:'GET',
+    })
+   const data = await response.json();
+   setUsers(data);
+  }
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+
+    
+  <h2>Login</h2>
+  <form  onClick={handleSubmit}>
+    {/* <p>{JSON.stringify(form)}</p> */}
+    <div className="input-group">
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        // id="username"
+        name="username"
+        onChange={handleForm}
+        
+        
+        required
+      />
     </div>
+    <div className="input-group">
+      <label htmlFor="password">Password</label>
+      <input
+         type="password"
+        // id="password"
+        name="password"
+        onChange={handleForm}
+        
+        
+        required
+      />
+    </div>
+    <div className="input-group">
+      <input type="submit" value="Login" className="btn" />
+    </div>
+    
+  </form>
+  {/* <div>
+    <ul>
+      {users.map(user=><li key={user._id} >{user.username},{user.password}</li>)}
+    </ul>
+  </div> */}
+</div>
+
+
+  
+    
   );
 }
 
